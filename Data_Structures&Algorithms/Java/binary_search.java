37368b7543ff5e57c24a947e9ea1af11044193be
ayushmanitiwari931@gmail.com <ayushmanitiwari931@gmail.com>
2025-10-15 09:30:36 +0530

    added the binary search algorithm

---
+++ Data_Structures&Algorithms/Java/binary_search.java
@@ -0,0 +1,16 @@
+class BinarySearch {
+    static int search(int[] arr, int x) {
+        int l = 0, r = arr.length - 1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (arr[m] == x) return m;
+            if (arr[m] < x) l = m + 1;
+            else r = m - 1;
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 5, 7, 9};
+        System.out.println(search(arr, 5));
+    }
+}
